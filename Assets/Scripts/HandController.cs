07f3ec9031c401991d28ad8c2fd1774479966b9f
Vijayalakshmi <vijayalakshmi.tg3@gmail.com>
2017-02-20 13:13:39 +0530

    Unity Controller Interaction

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HandController : MonoBehaviour {
+	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+	private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+	private SteamVR_TrackedObject trackedObj;
+
+	HashSet<IntractableItem> objectsHoveringOver = new HashSet<IntractableItem>();
+
+	private IntractableItem close_Object;
+	private IntractableItem Interacting_Object;
+
+	// Use this for initialization
+	void Start () {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller == null) {
+			Debug.Log("Controller not initialized");
+			return;
+		}
+
+		if (controller.GetPressUp (gripButton)) {
+			Debug.Log ("Grip button Pressed");
+			// find the close object to the controller when there are multiple intractable objects around
+			float minimun_distance = float.MaxValue;
+			float distance;
+			foreach (IntractableItem item in objectsHoveringOver) {
+				distance = (item.transform.position - transform.position).sqrMagnitude;
+				if (distance < minimun_distance) {
+					minimun_distance = distance;
+					close_Object = item;
+				}
+
+			}
+
+			Interacting_Object = close_Object;
+			close_Object = null;
+			// Pass the object Interaction from one controller to other controller
+			if (Interacting_Object) {
+				if (Interacting_Object.IsInteracting()) {
+					Interacting_Object.IntractionEnd (this);
+				}
+				Interacting_Object.IntractionBegin (this);
+			}
+		}
+
+		if (controller.GetPressDown (gripButton) && Interacting_Object != null) {
+			Debug.Log ("Grip button UnPressed");
+			Interacting_Object.IntractionEnd (this);
+		}
+
+	}
+
+
+	// Adds all colliding items to a HashSet for processing which is closest
+	private void OnTriggerEnter(Collider collider) {
+		Debug.Log("Entered");
+		IntractableItem collidedObject = collider.GetComponent<IntractableItem> ();
+		if (collidedObject) {
+			objectsHoveringOver.Add (collidedObject);
+		}
+	}
+
+	// Remove all items no longer colliding with to avoid further processing
+	private void OnTriggerExit(Collider collider) {
+		Debug.Log("Exit");
+		IntractableItem collidedObject = collider.GetComponent<IntractableItem> ();
+		if (collidedObject) {
+			objectsHoveringOver.Remove (collidedObject);
+		}
+	}
+}
---
+++ Assets/Scripts/IntractableItem.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntractableItem : MonoBehaviour {
+	public Rigidbody rigidbody;
+	private bool current_interact;
+	private Vector3 posvector;
+	private HandController attachedController;
+	private Transform interactionPoint;
+
+	private float rotation_factor = 300f;
+	private float velocity_factor = 18000f;
+	private Quaternion rotation;
+	private float angle;
+	private Vector3 axis;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		interactionPoint = new GameObject ().transform;
+
+		// rotation_factor and velocity_factor dependent on mass of the object
+		rotation_factor /= rigidbody.mass;
+		velocity_factor /= rigidbody.mass;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (attachedController && current_interact) {
+			posvector = attachedController.transform.position - interactionPoint.position;
+			this.rigidbody.velocity = posvector * velocity_factor * Time.fixedDeltaTime;
+		
+			rotation = attachedController.transform.rotation * Quaternion.Inverse (interactionPoint.rotation);
+			rotation.ToAngleAxis (out angle, out axis);
+
+			if (angle > 180) {
+				angle -= 360;
+			}
+
+			this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotation_factor;
+		}
+	}
+
+	public void IntractionBegin(HandController Hand){
+		attachedController = Hand;
+		interactionPoint.position = Hand.transform.position;
+		interactionPoint.rotation = Hand.transform.rotation;
+		interactionPoint.SetParent (transform, true);
+		current_interact = true;
+
+	}
+
+	public void IntractionEnd(HandController Hand){
+		if (Hand == attachedController) {
+			attachedController = null;
+			current_interact = false;
+		}
+	}
+
+	public bool IsInteracting(){
+		return current_interact;
+	}
+
+	public void OnDestroy(){
+	// destroy the empty game object
+		if (interactionPoint) {
+			Destroy (interactionPoint.gameObject);
+		}
+	}
+}
